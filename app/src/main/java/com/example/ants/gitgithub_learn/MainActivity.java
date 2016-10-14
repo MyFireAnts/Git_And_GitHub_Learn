5550e1f08e4c27e1d1020d42dcab8b95c3741a91
FireAnts <MyFireAnts@163.com>
2016-10-14 11:13:00 +0800

    第二次改变

--- app/src/main/java/com/example/ants/gitgithub_learn/MainActivity.java
+++ app/src/main/java/com/example/ants/gitgithub_learn/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.ants.gitgithub_learn;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 第二次测试
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
